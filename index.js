44909a35818e85bd269283a98bb07953023c9150
Sindre Sorhus <sindresorhus@gmail.com>
2014-10-10 01:27:18 +0200

    simplify code
    fixes #3

--- index.js
+++ index.js
@@ -4,40 +4,28 @@ var execFile = require('child_process').execFile;
 var xdgEmptyTrash = require('xdg-empty-trash');
 var runApplescript = require('run-applescript');
 
-function osx(cb) {
-	runApplescript('tell app "Finder" to empty trash', cb);
-}
-
-function linux(cb) {
-	xdgEmptyTrash(cb);
-}
-
-function win(cb) {
-	execFile('./nircmdc.exe', ['emptybin'], {
-		cwd: path.join(__dirname, 'vendor')
-	}, function (err) {
-		// NirCmd exits with this weird code even though it worked
-		if (err && err.code !== 4207175) {
-			cb(err);
-			return;
-		}
-
-		cb();
-	});
-}
-
 module.exports = function (cb) {
 	cb = cb || function () {};
 
 	if (process.platform === 'darwin') {
-		osx(cb);
+		runApplescript('tell app "Finder" to empty trash', cb);
 		return;
 	}
 
 	if (process.platform === 'win32') {
-		win(cb);
+		execFile('./nircmdc.exe', ['emptybin'], {
+			cwd: path.join(__dirname, 'vendor')
+		}, function (err) {
+			// NirCmd exits with this weird code even though it worked
+			if (err && err.code !== 4207175) {
+				cb(err);
+				return;
+			}
+
+			cb();
+		});
 		return;
 	}
 
-	linux(cb);
+	xdgEmptyTrash(cb);
 };
