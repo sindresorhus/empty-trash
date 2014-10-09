09f0f060bca20544e8f85b78ec1dffe4e0c49c74
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-09 13:23:55 +0200

    Close GH-2: Use xdg-empty-trash module for Linux.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var path = require('path');
 var execFile = require('child_process').execFile;
+var xdgEmptyTrash = require('xdg-empty-trash');
 var runApplescript = require('run-applescript');
 
 function osx(cb) {
@@ -8,11 +9,7 @@ function osx(cb) {
 }
 
 function linux(cb) {
-	execFile('./trash-empty', {
-		cwd: path.join(__dirname, 'vendor')
-	}, function (err) {
-		cb(err);
-	});
+	xdgEmptyTrash(cb);
 }
 
 function win(cb) {
