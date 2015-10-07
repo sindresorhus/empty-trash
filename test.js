892ac7480842cc3b93065b507f877d5f0866bd9c
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-07 17:06:00 +0700

    switch to AVA for testing
    https://github.com/sindresorhus/ava

--- test.js
+++ test.js
@@ -1,26 +1,25 @@
-'use strict';
-var fs = require('fs');
-var path = require('path');
-var assert = require('assert');
-var trash = require('trash');
-var userHome = require('user-home');
-var pathExists = require('path-exists');
-var emptyTrash = require('./');
+import fs from 'fs';
+import path from 'path';
+import test from 'ava';
+import trash from 'trash';
+import userHome from 'user-home';
+import pathExists from 'path-exists';
+import fn from './';
 
-it('empty trash', function (cb) {
-	var file = 'emptytrashfixture';
-	var trashFile = path.join(userHome, '.Trash', file);
+test(t => {
+	const file = 'emptytrashfixture';
+	const trashFile = path.join(userHome, '.Trash', file);
 
 	fs.writeFileSync(file, '');
 
-	trash([file], function (err) {
-		assert(!err, err);
-		assert(pathExists.sync(trashFile));
+	trash([file], err => {
+		t.ifError(err);
+		t.true(pathExists.sync(trashFile));
 
-		emptyTrash(function () {
-			assert(!err, err);
-			assert(!pathExists.sync(trashFile));
-			cb();
+		fn(() => {
+			t.ifError(err);
+			t.false(pathExists.sync(trashFile));
+			t.end();
 		});
 	});
 });
