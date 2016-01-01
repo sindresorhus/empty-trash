1000bdbeeb015f33a9f6936cc8f03c3ee9d6c7a5
Tom <tborychowski@gmail.com>
2016-01-01 22:25:00 +0000

    fix lint errors

--- index.js
+++ index.js
@@ -8,23 +8,33 @@ const pify = require('pify');
 const rimraf = require('rimraf');
 const df = require('@sindresorhus/df');
 
-
-function getTrash (drives) {
-	let uid = process.getuid();
-	return drives.map((d) => {
+function getTrash(drives) {
+	const uid = process.getuid();
+	return drives.map(d => {
 		// for root partition - get ~/.local/share/Trash
-		if (d.mountpoint === '/') return path.join(os.homedir(), '.local', 'share', 'Trash');
+		if (d.mountpoint === '/') {
+			return path.join(os.homedir(), '.local', 'share', 'Trash');
+		}
 		// for other e.g. /mnt/.Trash-1000
-		return path.join(d.mountpoint, '.Trash-' + uid);
+		return path.join(d.mountpoint, `.Trash-${uid}`);
+	})
+	// make sure path exists
+	.filter(d => {
+		let exists;
+		try {
+			fs.accessSync(d, fs.F_OK);
+			exists = true;
+		} catch (e) {
+			exists = false;
+		}
+		return exists;
 	});
 }
 
-function emptyTrash (pth) {
+function emptyTrash(pth) {
 	return pify(fs.readdir)(pth)
-		.then((files) => Promise.all(files.map((file) => pify(rimraf)(path.join(pth, file)))))
-		.catch(e => {})	// when path not found
+		.then(files => Promise.all(files.map(file => pify(rimraf)(path.join(pth, file)))));
 }
-
 
 module.exports = () => {
 	if (process.platform === 'darwin') {
@@ -35,5 +45,5 @@ module.exports = () => {
 		return pify(childProcess.execFile)(path.join(__dirname, 'lib', 'empty-recycle-bin.exe'));
 	}
 
-	return pify(df)().then(getTrash).then((trashes) => Promise.all(trashes.map(emptyTrash)));
+	return pify(df)().then(getTrash).then(trashes => Promise.all(trashes.map(emptyTrash)));
 };
