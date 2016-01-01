a4478dbb8e1cf8bc0139130ddb6aa9b4efe856eb
Tom <tborychowski@gmail.com>
2016-01-01 17:13:46 +0000

    use df instead of drivelist

--- index.js
+++ index.js
@@ -6,37 +6,23 @@ const childProcess = require('child_process');
 const runApplescript = require('run-applescript');
 const pify = require('pify');
 const rimraf = require('rimraf');
-const drivelist = require('drivelist');
+const df = require('@sindresorhus/df');
 
 
-function getDisks () {
-	return new Promise((resolve, reject) =>
-		drivelist.list((err, disks) => err ? reject(err) : resolve(disks))
-	)
-}
-
-
-function getTrashes (disks) {
+function getTrash (drives) {
 	let uid = process.getuid();
-	let homedir = os.homedir();
-	return disks
-		.filter(d => !!d.mountpoint)
-		.map(d => {
-			let mnt = d.mountpoint.replace(/\,$/, '');	// bug in drivelist adds comma to name
-
-			// for root partition - get ~/.local/share/Trash
-			if (mnt === '/') return path.join(homedir, '.local', 'share', 'Trash');
-
-			// for other e.g. /mnt/.Trash-1000
-			return path.join(mnt, '.Trash-' + uid);
-		});
+	return drives.map((d) => {
+		// for root partition - get ~/.local/share/Trash
+		if (d.mountpoint === '/') return path.join(os.homedir(), '.local', 'share', 'Trash');
+		// for other e.g. /mnt/.Trash-1000
+		return path.join(d.mountpoint, '.Trash-' + uid);
+	});
 }
 
 function emptyTrash (pth) {
-	return pify(fs.readdir)(pth).then(files => {
-		let remove = files.map(file => pify(rimraf)(path.join(pth, file)));
-		return Promise.all(remove);
-	});
+	return pify(fs.readdir)(pth)
+		.then((files) => Promise.all(files.map((file) => pify(rimraf)(path.join(pth, file)))))
+		.catch(e => {})	// when path not found
 }
 
 
@@ -49,7 +35,5 @@ module.exports = () => {
 		return pify(childProcess.execFile)(path.join(__dirname, 'lib', 'empty-recycle-bin.exe'));
 	}
 
-	return getDisks().then(getTrashes).then(trashes =>
-		Promise.all(trashes.map(emptyTrash))
-	);
+	return pify(df)().then(getTrash).then((trashes) => Promise.all(trashes.map(emptyTrash)));
 };
